5e5dc215aa2ad0d77dcb2ae3127477ac64f76180
dannilo12 <danteking1@gmail.com>
2022-11-09 21:07:22 -0300

    Entrega desenvolvida na recuperação dia 09/11/2022

---
+++ script.js
@@ -0,0 +1,8 @@
+function exibirDados(){
+    let email = document.getElementById("campo-email").value;
+
+    let senha = document.getElementById("campo-senha").value;
+
+    console.log("Email capturado:", email);
+    console.log("Senha capturada", senha);
+}
